907e82cd4863661f5858ae7c9afb4ce0fa12ab31
Pravesh Tora <pravesh.tora@gmail.com>
2019-08-14 03:16:30 +0530

    refactor:Updated Best score with react hooks

--- src/components/bestScore.js
+++ src/components/bestScore.js
@@ -1,45 +1,26 @@
-import React, { Component } from 'react';
-class BestScore extends Component {
-
-    constructor(props) {
-        super(props);
-        console.log(props);
-        let storage = window.localStorage;
-        let bestScore = storage.getItem('bestScore');
-        if (!bestScore) {
-            storage.setItem('bestScore',0);
-        }
-        console.log('bestScore' + bestScore);
-        if(bestScore) {
-        this.state = {
-            bestScore : bestScore
-        }} else {
-            this.state = {
-                bestScore : 0
-            }
-        }
+import React, { useState, useEffect } from 'react';
+const BestScore = (props) => {
+    let storage = window.localStorage;
+    let bestScoreLocal = storage.getItem('bestScore');
+    if (!bestScoreLocal) {
+        storage.setItem('bestScore', 0);
+        bestScoreLocal = 0;
     }
+    const [bestScore, setBestScore] = useState(bestScoreLocal);
 
-    componentDidUpdate =() => {
-        console.log("Current  "+ this.props.currentScore)
+    useEffect(() => {
         let storage = window.localStorage;
         let bestScore = storage.getItem('bestScore');
-        if (this.props.currentScore > bestScore) {
+        if (props.currentScore > bestScore) {
             let storage = window.localStorage;
-            storage.setItem('bestScore',this.props.currentScore)
-            this.setState(()=> {
-                return ({
-                    bestScore : this.props.currentScore
-                })
-            })
+            storage.setItem('bestScore', props.currentScore)
+            setBestScore(props.currentScore);
         }
-    }
+    })
 
-    render() {
-        return (
-            <span> Best Score {this.state.bestScore}</span>
-        );
-    }
+    return (
+        <span> Best Score {bestScore}</span>
+    );
 
 }
 
